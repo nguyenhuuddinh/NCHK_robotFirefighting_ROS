2a3c69b631ab7b32698977ac48bf5ca6fada14bd
HuuDinh <dinhnguyenhuu65@gmail.com>
2026-08-27 19:08:22 +0700

    style: fix ament_uncrustify divergence in camsense_x1_node.cpp
    - Fixes code style to meet ROS 2 ament_uncrustify standards
    - Closes L20-1

--- ros2_ws/src/camsense_x1/src/camsense_x1_node.cpp
+++ ros2_ws/src/camsense_x1/src/camsense_x1_node.cpp
@@ -18,10 +18,11 @@
 
 #include "camsense_x1/camsense_x1.hpp"
 
-int main(int argc, char **argv) {
+int main(int argc, char ** argv)
+{
   rclcpp::init(argc, argv);
   auto node =
-      std::make_shared<CamsenseX1>("camsense_x1_node", rclcpp::NodeOptions());
+    std::make_shared<CamsenseX1>("camsense_x1_node", rclcpp::NodeOptions());
   rclcpp::spin(node);
   rclcpp::shutdown();
   node = nullptr;
